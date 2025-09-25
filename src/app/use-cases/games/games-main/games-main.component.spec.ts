eee79ab6cede7681368f3eed4f048924a2049913
hoaC0 <hoachau1010@gmail.com>
2025-09-25 17:49:21 +0200

    Added Video

--- src/app/use-cases/games/games-main/games-main.component.spec.ts
+++ src/app/use-cases/games/games-main/games-main.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GamesComponent } from './games.component';
+import { GamesMainComponent } from './games-main.component';
 
-describe('GamesComponent', () => {
-  let component: GamesComponent;
-  let fixture: ComponentFixture<GamesComponent>;
+describe('GamesMainComponent', () => {
+  let component: GamesMainComponent;
+  let fixture: ComponentFixture<GamesMainComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GamesComponent]
+      imports: [GamesMainComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(GamesComponent);
+    fixture = TestBed.createComponent(GamesMainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/use-cases/games/games-main/games-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games-main',
+  imports: [],
+  templateUrl: './games-main.component.html',
+  styleUrl: './games-main.component.less'
+})
+export class GamesMainComponent {
+
+}
