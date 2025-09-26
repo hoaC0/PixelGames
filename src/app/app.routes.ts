a47cf1f515909ee188335a87c85facacb94ba698
hoaC0 <hoachau1010@gmail.com>
2025-09-26 13:33:20 +0200

    Signals added ( first time )

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './use-cases/home/home/home';
+import { GamesMainComponent } from './use-cases/games/games-main/games-main.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", component: Home },
+    { path: "games", component: GamesMainComponent},
     { path: "**", redirectTo: "/home" }
 ];
--- src/app/use-cases/home/home/home.ts
+++ src/app/use-cases/home/home/home.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Header } from '../../shared/components/header/header';
 import { HomeMainSection } from "../home-main-section/home-main-section";
 import { HomeGamesSection } from "../home-games-section/home-games-section";
+import { signal,ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,32 @@ import { HomeGamesSection } from "../home-games-section/home-games-section";
   templateUrl: './home.html',
   styleUrl: './home.less'
 })
-export class Home {
+export class Home implements OnInit {
+
+  ngOnInit() {
+   
+  }
+
+  // can only be "online" or "offline"
+  userstatus = signal<"online" | "offline">("offline");
+
+  toggleOn() {
+    this.userstatus.set("online");
+    console.log("Status:", this.userstatus());
+    // () because signal<>()
+  }
+
+  toggleOff() {
+    this.userstatus.set("offline");
+    console.log("Status:", this.userstatus());
+  }
+
+  toggle() {
+    if (this.userstatus() === "online") {
+      this.toggleOff();
+    } else {
+      this.toggleOn();
+    }
+  }
 
 }
