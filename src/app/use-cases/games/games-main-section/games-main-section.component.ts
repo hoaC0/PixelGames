c5b7677f7ae7234ce9262e6896fcc8448f681b4f
hoaC0 <hoachau1010@gmail.com>
2025-10-14 12:08:58 +0200

    Carousell dialog feature added

--- src/app/use-cases/games/games-main-section/games-main-section.component.ts
+++ src/app/use-cases/games/games-main-section/games-main-section.component.ts
@@ -1,17 +1,19 @@
-import { Component, computed, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, OnDestroy } from '@angular/core';
 import { GameStore } from '../store/games.store';
 import { Game } from '../../../model/game-store.model';
+import { LoadingScreen } from "../../shared/components/loading-screen/loading-screen";
 
 @Component({
   selector: 'app-games-main-section',
-  imports: [],
+  imports: [LoadingScreen],
   templateUrl: './games-main-section.component.html',
   styleUrl: './games-main-section.component.less'
 })
-export class GamesMainSectionComponent implements OnInit {
+export class GamesMainSectionComponent implements OnInit, OnDestroy {
   
   store = inject(GameStore);
   Math = Math;
+  private carouselInterval: any;
 
   visibleGames = computed(() => {
     const start = this.store.currentPage() * this.store.gamesPerPage();
@@ -21,6 +23,28 @@ export class GamesMainSectionComponent implements OnInit {
 
   ngOnInit() {
     this.store.loadAllGames();
+    this.startCarousel();
+  }
+
+  ngOnDestroy() {
+    this.stopCarousel();
+  }
+
+  startCarousel() {
+    this.carouselInterval = setInterval(() => {
+      const maxPage = Math.ceil(this.store.games().length / this.store.gamesPerPage()) - 1;
+      if (this.store.currentPage() < maxPage) {
+        this.store.nextPage();
+      } else {
+        this.store.goToPage(0);
+      }
+    }, 8000); // 8 seconds
+  }
+
+  stopCarousel() {
+    if (this.carouselInterval) {
+      clearInterval(this.carouselInterval);
+    }
   }
 
   openInfo(game: Game) {
